85bb9b92d136adca1cdba73dd6d06cc03b0351d5
“Julio <juliorhode@gmail.com>
2019-12-08 02:38:46 -0400

    Continuacion del curso 8-12-2019

---
+++ Eventos/Galeria de Imagenes/gallery-start/main.js
@@ -0,0 +1,40 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+    /* Looping through images */
+    for(let i = 1; i < 6; i++){
+        const newImage = document.createElement('img');
+        newImage.setAttribute('src', 'images/pic'+i+'.jpg');
+        thumbBar.appendChild(newImage);
+        newImage.onclick = function(e){
+            console.log(e.target.getAttribute('src'));
+            
+            const imgSrc = e.target.getAttribute('src');
+            displayImage(imgSrc);
+        }
+    }
+    function displayImage(imgSrc) {
+        console.log(imgSrc);
+        
+        displayedImage.setAttribute('src', imgSrc);
+    }
+
+
+/* Wiring up the Darken/Lighten button */
+    btn.onclick = function(){
+        const btnClass = btn.getAttribute('class');
+        console.log(btnClass);
+        if (btnClass ==='dark') {
+            btn.setAttribute('class','light');
+            btn.textContent = 'Claro';
+            overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        }else{
+            btn.setAttribute('class','dark');
+            btn.textContent = 'Oscuro';
+            overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+        }
+        
+    }
